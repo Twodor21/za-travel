9292550834a90f19518a53b3dffd2e5f17f62122
Twodor21 <tudorpopa92@gmail.com>
2024-04-17 20:57:14 +0300

    fix: app dialog paddings

--- src/features/ui/AppDialog.tsx
+++ src/features/ui/AppDialog.tsx
@@ -61,12 +61,13 @@ export default function AppDialog({
         sx={{
           pt: 8.75,
           px: { xs: MOBILE_PADDING_X, md: DESKTOP_PADDING_X },
+          pb: 3,
         }}
       >
         {title}
       </Typography>
       <DialogContent
-        sx={{ px: { xs: MOBILE_PADDING_X, md: DESKTOP_PADDING_X }, py: 3 }}
+        sx={{ px: { xs: MOBILE_PADDING_X, md: DESKTOP_PADDING_X }, py: 0 }}
       >
         {children}
       </DialogContent>
@@ -74,7 +75,7 @@ export default function AppDialog({
         sx={{
           px: { xs: MOBILE_PADDING_X, md: DESKTOP_PADDING_X },
           pb: { xs: MOBILE_PADDING_Y, md: DESKTOP_PADDING_Y },
-          pt: 0,
+          pt: 3,
         }}
       >
         <AppButton fullWidth onClick={onPrimaryButtonClick}>
